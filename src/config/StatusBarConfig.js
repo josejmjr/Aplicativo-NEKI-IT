ff992fea916476435b88c3b03e0467ea3bacdc20
José Junior <38918030+josejmjr@users.noreply.github.com>
2019-12-07 17:02:28 -0300

    Gradient

--- src/config/StatusBarConfig.js
+++ src/config/StatusBarConfig.js
@@ -1,5 +1,6 @@
 import {StatusBar} from 'react-native';
 
-StatusBar.setBackgroundColor('#1a5065'); // fazer com que a barra de status fique da mesma cor que o header
 
+//StatusBar.setBackgroundColor('#1295a3'); // fazer com que a barra de status fique da mesma cor que o header
+StatusBar.setBackgroundColor('#04b6b8');
 StatusBar.setBarStyle('light-content');
